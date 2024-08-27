5a8546c67fc5f2330f63033bcf3968ab389e0d92
Waleska Oliveira <waleska.ziviani.peixoto@gmail.com>
2024-08-26 23:30:03 -0300

    Subindo para o Git

---
+++ CDV/app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/MainActivity.kt
@@ -0,0 +1,61 @@
+package br.edu.ifsp.scl.ads.pdm.cdv
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.cdv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        amb.abraBt.setOnClickListener{
+            Intent(this, AnotherActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo completo")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name), "onStart: Iniciando ciclo visível")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(getString(R.string.app_name), "onResume: Iniciando ciclo foreground")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(getString(R.string.app_name), "onPause: Finalizando ciclo foreground")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(getString(R.string.app_name), "onStop: Finalizando ciclo visível")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(getString(R.string.app_name), "onDestroy: Finalizando ciclo completo")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "onRestart: Preparando o onStart")
+    }
+
+
+}
